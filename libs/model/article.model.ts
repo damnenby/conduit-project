1fed0d7ebb82f18aedd20dba312167c46ee4f371
Your Name <your-email@example.com>
2026-04-29 18:44:24 +0200

    add article author

--- libs/model/article.model.ts
+++ libs/model/article.model.ts
@@ -1,5 +1,12 @@
 import * as z from 'zod';
 
+export const ProfileSchema = z.object({
+  username: z.string(),
+  bio: z.string().nullable(),
+  image: z.string().nullable(),
+  following: z.boolean(),
+});
+
 export const ArticleSchema = z.object({
   slug: z.string(),
   title: z.string(),
@@ -8,7 +15,10 @@ export const ArticleSchema = z.object({
   tagList: z.array(z.string()),
   createdAt: z.string(),
   updatedAt: z.string(),
+  favorited: z.boolean(),
   favoritesCount: z.number(),
+  author: ProfileSchema,
 });
 
+export type Profile = z.infer<typeof ProfileSchema>;
 export type Article = z.infer<typeof ArticleSchema>;
