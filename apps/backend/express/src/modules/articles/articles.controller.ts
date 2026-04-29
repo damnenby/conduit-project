810ba40e8bdabe4bcc8751947964872ac8b84b4b
Your Name <your-email@example.com>
2026-04-29 18:25:01 +0200

    show article example

--- apps/backend/express/src/modules/articles/articles.controller.ts
+++ apps/backend/express/src/modules/articles/articles.controller.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express';
+import type { Article } from '@common/model';
 
 export const articlesRouter: Router = Router();
 
+const articles: Article[] = [
+  {
+    slug: 'first-conduit-article',
+    title: 'First Conduit Article',
+    description: 'This is a first example article.',
+    body: 'This article is here for testing the new articles endpoint.',
+    tagList: ['intro'],
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    favoritesCount: 0,
+  },
+];
+
 articlesRouter.get('/', (_req, res) => {
   return res.json({
-    articles: [],
-    articlesCount: 0,
+    articles,
+    articlesCount: articles.length,
   });
 });
