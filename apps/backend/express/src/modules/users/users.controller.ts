b1848560c18a4a2015d07a925873ca0bb4a1bf73
Your Name <your-email@example.com>
2026-04-30 18:31:35 +0200

    add register route

---
+++ apps/backend/express/src/modules/users/users.controller.ts
@@ -0,0 +1,48 @@
+import { Router } from 'express';
+import type { User } from '@common/model';
+import { createToken } from '../../utils/jwt';
+import { hashPassword } from '../../utils/password';
+
+export const usersRouter: Router = Router();
+
+const users: Array<User & { id: number; passwordHash: string }> = [];
+
+usersRouter.post('/', async (req, res) => {
+  const username = req.body?.user?.username;
+  const email = req.body?.user?.email;
+  const password = req.body?.user?.password;
+
+  if (!username || !email || !password) {
+    return res.status(422).json({
+      errors: {
+        body: ['Username, email and password are required'],
+      },
+    });
+  }
+
+  const id = users.length + 1;
+  const passwordHash = await hashPassword(password);
+  const token = createToken(id);
+
+  const user = {
+    id,
+    email,
+    token,
+    username,
+    bio: null,
+    image: null,
+    passwordHash,
+  };
+
+  users.push(user);
+
+  return res.status(201).json({
+    user: {
+      email: user.email,
+      token: user.token,
+      username: user.username,
+      bio: user.bio,
+      image: user.image,
+    },
+  });
+});
