970f764fe7dcac3310bcbcc4af6170513bc39ab9
Your Name <your-email@example.com>
2026-04-30 20:25:21 +0200

    show auth in nav

--- apps/frontend/vue/src/App.vue
+++ apps/frontend/vue/src/App.vue
@@ -2,8 +2,16 @@
   <header>
     <nav>
       <RouterLink to="/">Conduit</RouterLink>
-      <RouterLink to="/login">Login</RouterLink>
-      <RouterLink to="/register">Register</RouterLink>
+
+      <template v-if="isLoggedIn">
+        <span>{{ user?.username }}</span>
+        <button @click="logout">Logout</button>
+      </template>
+
+      <template v-else>
+        <RouterLink to="/login">Login</RouterLink>
+        <RouterLink to="/register">Register</RouterLink>
+      </template>
     </nav>
   </header>
 
@@ -14,6 +22,9 @@
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { useAuth } from './composables/useAuth'
+
+const { user, isLoggedIn, logout } = useAuth()
 </script>
 
 <style scoped></style>
