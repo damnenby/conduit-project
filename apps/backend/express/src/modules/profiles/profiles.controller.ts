b2173f26d5bbf48f52394ee4c7073c1fadfa4827
damnenby <damnenby@gmail.com>
2026-05-03 15:59:30 +0200

    add follow button

--- apps/backend/express/src/modules/profiles/profiles.controller.ts
+++ apps/backend/express/src/modules/profiles/profiles.controller.ts
@@ -25,3 +25,35 @@ profilesRouter.get('/:username', (req, res) => {
 
   return res.json({ profile });
 });
+
+profilesRouter.post('/:username/follow', (req, res) => {
+  const profile = profiles.find((item) => item.username === req.params.username);
+
+  if (!profile) {
+    return res.status(404).json({
+      errors: {
+        body: ['Profile not found'],
+      },
+    });
+  }
+
+  profile.following = true;
+
+  return res.json({ profile });
+});
+
+profilesRouter.delete('/:username/follow', (req, res) => {
+  const profile = profiles.find((item) => item.username === req.params.username);
+
+  if (!profile) {
+    return res.status(404).json({
+      errors: {
+        body: ['Profile not found'],
+      },
+    });
+  }
+
+  profile.following = false;
+
+  return res.json({ profile });
+});
